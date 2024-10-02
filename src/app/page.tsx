a6e5dc8d8b9ad6bf05bfbad4fe407019423a03fa
Lucas Siqueira <wefit.siqueira@voeazul.com.br>
2024-10-01 22:02:27 -0300

    new cp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import styles from "./page.module.css";
 
-import { ButtonComponente } from "@shared/my-app";
+import { ButtonComponente, Typography } from "@shared/my-app";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <ButtonComponente />
+      <Typography />
     </div>
   );
 }
